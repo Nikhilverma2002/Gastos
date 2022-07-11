71840330292d4b9cf3cee2f18b99067e9235675b
Nikhil Verma <nikhilverma.nv345@gmail.com>
2022-07-11 18:39:10 +0530

    First commit

---
+++ app/src/main/java/com/gastos/gastos2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.gastos.gastos2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scanner);
+
+        //#10B981
+    }
+}

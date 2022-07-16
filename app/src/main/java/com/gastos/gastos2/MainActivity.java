2a7df8a5e6d75e604840604e731c901eaf166e26
Nikhil Verma <nikhilverma.nv345@gmail.com>
2022-07-17 01:18:39 +0530

    Second Commit

--- app/src/main/java/com/gastos/gastos2/MainActivity.java
+++ app/src/main/java/com/gastos/gastos2/MainActivity.java
@@ -1,15 +1,22 @@
 package com.gastos.gastos2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.scanner);
+        setContentView(R.layout.select_package);
+
+        Window window = MainActivity.this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(MainActivity.this, R.color.white));
 
         //#10B981
     }
